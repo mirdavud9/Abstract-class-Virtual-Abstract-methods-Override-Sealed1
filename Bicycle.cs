79e4cbd9dec13b9267605eb1477b27d3d51e155e
DevDavud <mirdavudvm-bp217@code.edu.az>
2024-10-18 23:32:05 +0400

    Add files via upload

---
+++ Bicycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AbstractClass
+{
+    internal class Bicycle : Vehicle
+    {
+        public string Type { get; set; }
+        public override string GetInfo()
+        {
+            return base.GetInfo() + $", Type:{Type}";
+        }
+        public override string DefineNatureHarmness()
+        {
+            return "nothing";
+        }
+    }
+
+}
---
+++ Vehicle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_class__Virtual__Abstract_methods__Override__Sealed
+{
+    internal class Vehicle
+    {
+    }
+}
